d9186de18c9683d38c78a910876d96a4383c5095
Maurício Dias de Carvalho Machado <mauricio.machado@antt.gov.br>
2025-02-26 15:57:00 -0300

    Criação das Entidades de Venda e Item de Venda

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -0,0 +1,32 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Sale
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string SaleNumber { get; set; }
+        public DateTime SaleDate { get; set; }
+        public string Client { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Branch { get; set; }
+        public List<SaleItem> Items { get; set; } = new();
+        public bool IsCancelled { get; set; }
+
+        public void CancelSale()
+        {
+            IsCancelled = true;
+            foreach (var item in Items)
+            {
+                item.CancelItem();
+            }
+        }
+
+        public void RecalculateTotal()
+        {
+            TotalAmount = 0;
+            foreach (var item in Items)
+            {
+                TotalAmount += item.TotalPrice;
+            }
+        }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/SaleItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class SaleItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid SaleId { get; set; }
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public decimal TotalPrice { get; set; }
+        public bool IsCancelled { get; set; }
+
+        public void ApplyDiscount()
+        {
+            if (Quantity < 4)
+                Discount = 0;
+            else if (Quantity >= 4 && Quantity < 10)
+                Discount = 0.10m;
+            else if (Quantity >= 10 && Quantity <= 20)
+                Discount = 0.20m;
+            else
+                throw new InvalidOperationException("Quantity greater than 20 is not allowed.");
+
+            TotalPrice = (UnitPrice * Quantity) * (1 - Discount);
+        }
+
+        public void CancelItem()
+        {
+            IsCancelled = true;
+            TotalPrice = 0;
+        }
+    }
+}
